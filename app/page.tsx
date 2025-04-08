cb647de41fa5663c7c14dc4763ca4d82a3ce6253
MuhammadShameel <mshameelkz@gmail.com>
2025-04-08 15:07:31 +0500

    remove unused declaration

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
+// import Image from "next/image";
 // import Landing from "./components/Landing";
-import Landing from "./components/landing";
-import BlogSlide from "./components/BlogSlide";
-import HowItWorks from "./components/HowItWorks";
-import ProductDetail from "./components/ProductDetail";
+// import Landing from "./components/landing";
+// import BlogSlide from "./components/BlogSlide";
+// import HowItWorks from "./components/HowItWorks";
 import Products from "./components/Products";
 
 export default function Home() {
